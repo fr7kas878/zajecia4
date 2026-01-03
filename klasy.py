556849e03c7c3eae7313ad3705412030c001dd08
fr7kas878 <fr7kas@gmail.com>
2026-01-03 10:06:39 +0000

    push z zajec

---
+++ klasy.py
@@ -0,0 +1,19 @@
+class Czlowiek:
+
+
+
+    def __init__(self): #akt istnienia #metoda czyli funkcja wewnatrz klasy
+
+
+
+
+#ctrr+ back slash - komentowanie # bloku
+print (type(Czlowiek))
+
+print (dir (Czlowiek)) #pokaz folder a tu sa pozostale klasy ukryte z __nazwa__
+
+# __init__ konstruktor, ktroy powoluje do istnienia dany obiekt
+
+
+adam = Czlowiek() #obiekt w klasie Czlowiek
+print (type(adam))
